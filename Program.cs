34c67ef79d04ce7ef52f7bffc52f7b1dc4fa532f
Bura-Kevin <kevin.bura@ynov.com>
2026-04-10 10:41:37 +0200

    x
    ground beta v 0.1 ( incomplete )

--- Program.cs
+++ Program.cs
@@ -12,7 +12,29 @@ class Program
 
         Rectangle cube = new Rectangle(100, 500, 50, 50);
         float speed = 200f;
-        Rectangle ground = new Rectangle(0, 550, screenWidth, 50);
+        // platforms
+        Rectangle ground = new Rectangle(0, 750, 1200, 50);
+
+        Rectangle plat1 = new Rectangle(0, 500, 200, 20);
+        Rectangle plat2 = new Rectangle(250, 300, 400, 20);
+
+        Rectangle plat3 = new Rectangle(700, 600, 400, 20);
+
+        // small jump blocks
+        Rectangle b1 = new Rectangle(700, 300, 50, 50);
+        Rectangle b2 = new Rectangle(850, 350, 50, 50);
+        Rectangle b3 = new Rectangle(1000, 500, 50, 50);
+        Rectangle b4 = new Rectangle(1100, 650, 50, 50);
+
+        // ladder
+        Rectangle ladder = new Rectangle(850, 600, 50, 150);
+
+        float yVel = 0;
+        float gravity = 800f;
+        float jump = -350f;
+        bool onGround = false;
+
+        bool onLadder = false;
 
         while (!Raylib.WindowShouldClose())
         {
@@ -38,19 +60,78 @@ class Program
                 Raylib.ToggleFullscreen();
             }
 
+            // monte desands 
+            if (Raylib.CheckCollisionRecs(cube, ladder))
+            {
+                onLadder = true;
+            }
+            else
+            {
+                onLadder = false;
+            }
+            
+            // si on z pas, on tombe
+            if (!onLadder || !Raylib.IsKeyDown(KeyboardKey.Z))
+            {
+                yVel += gravity * deltaTime;
+            }
+
+            // climb
+            if (onLadder && Raylib.IsKeyDown(KeyboardKey.Z))
+            {
+                yVel = -150f; // go up
+            }
+
             // Movement
             if (Raylib.IsKeyDown(KeyboardKey.Left)) cube.X -= speed * deltaTime;
             if (Raylib.IsKeyDown(KeyboardKey.Right)) cube.X += speed * deltaTime;
 
-            // Ground collision
-            if (cube.Y + cube.Height > ground.Y)
-                cube.Y = ground.Y - cube.Height;
+            // gravity
+            yVel += gravity * deltaTime;
+            cube.Y += yVel * deltaTime;
 
-            // Drawing
+            
+            // ground
+            if (
+                yVel >= 0 && // only when falling
+                cube.X + cube.Width > ground.X &&
+                cube.X < ground.X + ground.Width &&
+                cube.Y + cube.Height >= ground.Y &&
+                cube.Y + cube.Height <= ground.Y + 10 
+            )
+            {
+                cube.Y = ground.Y - cube.Height;
+                yVel = 0;
+                onGround = true;
+            }
+            else
+            {
+                onGround = false;
+            }
+
+            // jump
+            if (Raylib.IsKeyPressed(KeyboardKey.Space) && onGround)
+            {
+                yVel = jump;
+            }
+
+            // affichage
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.SkyBlue);
 
-            Raylib.DrawRectangleRec(ground, Color.DarkGray);
+            Raylib.DrawRectangleRec(ground, Color.LightGray);
+
+            Raylib.DrawRectangleRec(plat1, Color.LightGray);
+            Raylib.DrawRectangleRec(plat2, Color.LightGray);
+            Raylib.DrawRectangleRec(plat3, Color.LightGray);
+
+            Raylib.DrawRectangleRec(b1, Color.Gray);
+            Raylib.DrawRectangleRec(b2, Color.Gray);
+            Raylib.DrawRectangleRec(b3, Color.Gray);
+            Raylib.DrawRectangleRec(b4, Color.Gray);
+
+            Raylib.DrawRectangleRec(ladder, Color.White);
+
             Raylib.DrawRectangleRec(cube, Color.Red);
 
             Raylib.DrawText("ALT + ENTER = Fullscreen", 10, 10, 20, Color.Black);
