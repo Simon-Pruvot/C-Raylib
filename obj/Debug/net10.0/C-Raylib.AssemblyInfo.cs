d2e4dfc1685325eecc55b6c472541b2ce356089e
Simon-Pruvot <simon.pruvot@ynov.com>
2026-05-04 22:34:45 +0200

    n

--- obj/Debug/net10.0/C-Raylib.AssemblyInfo.cs
+++ obj/Debug/net10.0/C-Raylib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C-Raylib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c5679ae35964f8e43dc61721da6fb5d9d201ddc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fbfc4096cf9c82622ce4c19b1b8411ba409f6d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("C-Raylib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C-Raylib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
